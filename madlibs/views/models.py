414462ef1f1c05cac3b48e0b5373d135d4148335
Pace Wasden <pacewasden@gmail.com>
2020-11-28 16:44:40 -0700

    Model set up

--- madlibs/views/models.py
+++ madlibs/views/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Words(models.Model):
+    def __str__(self):
+        return self.noun
+    
+    noun=models.CharField(max_length=100)
+    adjective=models.CharField(max_length=100)
+    place=models.CharField(max_length=100)
+    expression= models.CharField(max_length=100)
